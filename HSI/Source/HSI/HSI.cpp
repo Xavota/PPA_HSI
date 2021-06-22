b10799a582987cecad0c0f05c9576894179d1708
Diego Castellanos <57270722+Xavota@users.noreply.github.com>
2021-06-22 16:58:26 -0500

    Proyecto subido

---
+++ HSI/Source/HSI/HSI.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HSI.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HSI, "HSI" );
---
+++ HSI/Source/HSI/HSIGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HSIGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HSI_API AHSIGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
