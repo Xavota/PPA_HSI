926d273d4421325d70bbaaf8eb538dc20f5ea709
MSI\simam <59717749+Simeone0647@users.noreply.github.com>
2021-10-11 16:41:43 -0500

    Python Plugin

---
+++ HSI/Intermediate/Build/Win64/UE4Editor/Inc/HSI/generador.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef HSI_generador_generated_h
+#error "generador.generated.h already included, missing '#pragma once' in generador.h"
+#endif
+#define HSI_generador_generated_h
+
+#define HSI_Source_HSI_generador_h_15_SPARSE_DATA
+#define HSI_Source_HSI_generador_h_15_RPC_WRAPPERS
+#define HSI_Source_HSI_generador_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define HSI_Source_HSI_generador_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUgenerador(); \
+	friend struct Z_Construct_UClass_Ugenerador_Statics; \
+public: \
+	DECLARE_CLASS(Ugenerador, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/HSI"), NO_API) \
+	DECLARE_SERIALIZER(Ugenerador)
+
+
+#define HSI_Source_HSI_generador_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesUgenerador(); \
+	friend struct Z_Construct_UClass_Ugenerador_Statics; \
+public: \
+	DECLARE_CLASS(Ugenerador, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/HSI"), NO_API) \
+	DECLARE_SERIALIZER(Ugenerador)
+
+
+#define HSI_Source_HSI_generador_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API Ugenerador(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(Ugenerador) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, Ugenerador); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(Ugenerador); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API Ugenerador(Ugenerador&&); \
+	NO_API Ugenerador(const Ugenerador&); \
+public:
+
+
+#define HSI_Source_HSI_generador_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API Ugenerador(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API Ugenerador(Ugenerador&&); \
+	NO_API Ugenerador(const Ugenerador&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, Ugenerador); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(Ugenerador); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(Ugenerador)
+
+
+#define HSI_Source_HSI_generador_h_15_PRIVATE_PROPERTY_OFFSET
+#define HSI_Source_HSI_generador_h_12_PROLOG
+#define HSI_Source_HSI_generador_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	HSI_Source_HSI_generador_h_15_PRIVATE_PROPERTY_OFFSET \
+	HSI_Source_HSI_generador_h_15_SPARSE_DATA \
+	HSI_Source_HSI_generador_h_15_RPC_WRAPPERS \
+	HSI_Source_HSI_generador_h_15_INCLASS \
+	HSI_Source_HSI_generador_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define HSI_Source_HSI_generador_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	HSI_Source_HSI_generador_h_15_PRIVATE_PROPERTY_OFFSET \
+	HSI_Source_HSI_generador_h_15_SPARSE_DATA \
+	HSI_Source_HSI_generador_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	HSI_Source_HSI_generador_h_15_INCLASS_NO_PURE_DECLS \
+	HSI_Source_HSI_generador_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> HSI_API UClass* StaticClass<class Ugenerador>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID HSI_Source_HSI_generador_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
